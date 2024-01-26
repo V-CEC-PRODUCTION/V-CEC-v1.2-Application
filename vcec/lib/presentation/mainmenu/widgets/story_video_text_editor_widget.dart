56c353e73bd25efcc6bcc837ecf4022d2e0ee730
Abhishek <ksabhishek00@gmail.com>
2024-01-26 18:33:06 +0530

    video upload page done

---
+++ vcec/lib/presentation/mainmenu/widgets/story_video_text_editor_widget.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:text_editor/text_editor.dart';
+
+class StoryVideoTextEditor extends StatefulWidget {
+  StoryVideoTextEditor({Key? key}) : super(key: key);
+
+  @override
+  _StoryVideoTextEditorState createState() => _StoryVideoTextEditorState();
+}
+
+class _StoryVideoTextEditorState extends State<StoryVideoTextEditor> {
+  final fonts = [
+    'OpenSans',
+    'Billabong',
+    'GrandHotel',
+    'Oswald',
+    'Quicksand',
+    'BeautifulPeople',
+    'BeautyMountains',
+    'BiteChocolate',
+    'BlackberryJam',
+    'BunchBlossoms',
+    'CinderelaRegular',
+    'Countryside',
+    'Halimun',
+    'LemonJelly',
+    'QuiteMagicalRegular',
+    'Tomatoes',
+    'TropicalAsianDemoRegular',
+    'VeganStyle',
+  ];
+  TextStyle _textStyle = TextStyle(
+    fontSize: 50,
+    color: Colors.white,
+    fontFamily: 'Billabong',
+  );
+  String _text = '';
+  TextAlign _textAlign = TextAlign.center;
+
+  void _tapHandler(text, textStyle, textAlign) {
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: false,
+      transitionDuration: Duration(
+        milliseconds: 400,
+      ), // how long it takes to popup dialog after button click
+      pageBuilder: (_, __, ___) {
+        // your widget implementation
+        return Container(
+          color: Colors.black.withOpacity(0.4),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: SafeArea(
+              // top: false,
+              child: Container(
+                child: TextEditor(
+                  fonts: fonts,
+                  text: text,
+                  textStyle: textStyle,
+                  textAlingment: textAlign,
+                  minFontSize: 10,
+                  onEditCompleted: (style, align, text) {
+                    setState(() {
+                      _text = text;
+                      _textStyle = style;
+                      _textAlign = align;
+                    });
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  double top = 0;
+  double left = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: top,
+      left: left,
+      child: GestureDetector(
+        onTap: () => _tapHandler(_text, _textStyle, _textAlign),
+        onPanUpdate: (details) {
+          setState(() {
+            top += details.delta.dy;
+            left += details.delta.dx;
+          });
+        },
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.transparent,
+          child: Text(
+            _text,
+            style: _textStyle,
+            textAlign: _textAlign,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- vcec/lib/presentation/mainmenu/widgets/story_video_upload_widget.dart
+++ vcec/lib/presentation/mainmenu/widgets/story_video_upload_widget.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:vcec/presentation/mainmenu/widgets/story_video_text_editor_widget.dart';
 import 'package:video_player/video_player.dart';
 
 class StoryVideoUploadWidget extends StatefulWidget {
@@ -38,27 +39,63 @@ class _StoryVideoUploadWidgetState extends State<StoryVideoUploadWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          label: Icon(Icons.send),
+          backgroundColor: Colors.white,
+        ),
         body: SafeArea(
-          child: FutureBuilder(
-            future: _initializeVideoPlayerFuture,
-            builder: ((context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(35, 55, 35, 55),
-                  child: Container(
+          child: Stack(children: [
+            FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: ((context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Container(
                     height: _controller.value.size.height,
                     width: _controller.value.size.width,
                     child: VideoPlayer(_controller),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                      style: IconButton.styleFrom(
+                          backgroundColor: Colors.black.withOpacity(0.5))),
+                  IconButton(
+                    onPressed: () {
+                      StoryVideoTextEditor();
+                    },
+                    icon: Icon(
+                      CupertinoIcons.textformat,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.black.withOpacity(0.5),
+                    ),
                   ),
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            }),
-          ),
+                ],
+              ),
+            ),
+            StoryVideoTextEditor()
+          ]),
         ));
   }
 }
