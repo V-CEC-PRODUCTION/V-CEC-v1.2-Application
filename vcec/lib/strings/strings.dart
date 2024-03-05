9152da8407619a488a31b6f1846724d6650aada0
Nidhin Ninan <nidhinvninan29@gmail.com>
2024-03-05 15:00:03 +0530

    Sign up google Modified

--- vcec/lib/strings/strings.dart
+++ vcec/lib/strings/strings.dart
@@ -1,4 +1,4 @@
-const String baseUrl = "http://14.139.189.212/";
+const String baseUrl = "https://71e3-2401-4900-1cde-8df3-b5-b985-a33b-6e50.ngrok-free.app/";
 
 //buildrunner terminal command with watcher
 //flutter packages pub run build_runner watch --delete-conflicting-outputs
